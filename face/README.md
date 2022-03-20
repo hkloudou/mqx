27bdfa37baa0ea7fea7211646e6e9956619f9d96
sample <global-config@example.com>
2022-03-21 01:17:41 +0800

    ok, see you tomorrow

---
+++ face/README.md
@@ -0,0 +1,12 @@
+## face
+
+### Auth
+
+### ACL
+
+### Rule Engine
+
+### Store
+- Redis
+### Bridge
+- Nats
--- face/auth.go
+++ face/auth.go
@@ -1,9 +1,42 @@
 package face
 
-// Auth auth interface
-// mqtt.ConnectPacket
+import (
+	"errors"
+	"time"
+)
+
+type ErrAuthInvalid error
+
+var (
+	ErrAuthInvalidUserName ErrAuthInvalid = errors.New("mqx: auth failed, invalid userName")
+	ErrAuthInvalidPassword ErrAuthInvalid = errors.New("mqx: auth failed, invalid password")
+	ErrAuthInvalidClientIP ErrAuthInvalid = errors.New("mqx: auth failed, invalid clientIP")
+	ErrAuthInvalidExpired  ErrAuthInvalid = errors.New("mqx: auth failed, token expired")
+)
+
+type AuthRequest struct {
+	ClientId       string `json:"clientId"`       // reCommended to use deviceID
+	UserName       string `json:"userName"`       // reCommended to use userID `uint64`
+	PassWord       string `json:"passWord"`       // reCommended to use temporary password or http sessionID token
+	TlsServerName  string `json:"tlsServerName"`  // tls ShakeHande ServerName
+	TlsSubjectName string `json:"tlsSubjectName"` // tls PeerCertificates subject.CommonName
+	ClientIp       string `json:"clientIp"`       // real clientip
+}
+
+type AuthReply struct {
+	UserName  string `json:"userName"`
+	ExpiredAt int64  `json:"expiredAt"` // 0 if never expire
+}
+
+// Auth interface
+// default public mqtt account is mqtt:public
 type Auth interface {
-	Init()
-	Save(info *TransportInfo, ClientIdentifier string, userName string, passWord string)
-	Check(info *TransportInfo, ClientIdentifier string, userName string, passWord string)
+	Init() error
+	Options()
+	// call the function in your application
+	Update(req *AuthRequest, ttl time.Duration) error
+	//
+	Delete(req *AuthRequest) error
+	// when the mqtt broker receive a mqtt.Connect packet
+	Check(req *AuthRequest) (*AuthReply, error)
 }
